27277d03dd047372539a9c927b33374adbdd1e55
Bruno Henrique de Paula <orec247@gmail.com>
2021-04-26 11:15:26 -0300

    Finalizei aula 1

---
+++ Formação HTML && CSS3/JavaScript/Aula 1 - Introdução a Linguagem/1-meuPrimeiroPrograma.js
@@ -0,0 +1,13 @@
+console.log("Meu Primeiro programa: Trabalhando com variáveis");
+
+const idade = 20;
+
+console.log("idade");
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade+2;
+
+console.log(idadeSomada)
