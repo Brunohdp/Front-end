f22a02b387a098fe895991c48d22abb197c20f78
Bruno Henrique de Paula <78300872+Orecruta@users.noreply.github.com>
2021-06-11 11:15:45 -0300

    Terminei o Criando Elementos
    Aprendemos como criar um elemento em HTML dinamicamente de forma que ele não perca suas características de CSS e fique um abaixo do outro na sequência que foram adicionados.

--- Formação HTML && CSS3/JavaScript na Web/JS na Web - Manipule o DOM com JavaScript/Aula 3 – Adicionar Item na Lista/listaDeTarefas.js
+++ Formação HTML && CSS3/JavaScript na Web/JS na Web - Manipule o DOM com JavaScript/Aula 3 – Adicionar Item na Lista/listaDeTarefas.js
@@ -1,8 +1,16 @@
 const criarTarefa = (evento) => {
   evento.preventDefault()
+
+  const lista = document.querySelector('[data-list]')
   const input = document.querySelector('[data-form-input]')
   const valor = input.value
-  console.log(valor)
+
+  const tarefa = document.createElement('li')
+  tarefa.classList.add('task')
+  const conteudo = `<p class="content">${valor}</p>`
+
+  tarefa.innerHTML = conteudo
+  lista.appendChild(tarefa)
   input.value = " "
 }
 
