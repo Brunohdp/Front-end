0cb95e3252a663fc7e21038e6b7337ded4791185
Bruno Henrique de Paula <78300872+Orecruta@users.noreply.github.com>
2021-06-16 14:28:26 -0300

    Manipulando Array
    Aprendemos a criar arrays vazias e fazer push de informações para dentro delas, mantendo os dados armazenados no localStorage sem que os anteriores sejam apagados quando novos são inseridos, como era anteriormente.

--- Formação HTML && CSS3/JavaScript na Web/Armazenando Dados no Navegador/Aula 2 – LocalStorage/main.js
+++ Formação HTML && CSS3/JavaScript na Web/Armazenando Dados no Navegador/Aula 2 – LocalStorage/main.js
@@ -1,6 +1,8 @@
 import BotaoConclui from './componentes/concluiTarefa.js'
 import BotaoDeleta from './componentes/deletaTarefa.js'
 
+let tarefas = []
+
 const handleNovoItem = (evento) => {
   evento.preventDefault()
   const lista = document.querySelector('[data-list]')
@@ -20,7 +22,9 @@ const handleNovoItem = (evento) => {
 
   lista.appendChild(criaTarefa)
 
-  localStorage.setItem("tarefas", JSON.stringify(dados))
+  tarefas.push(dados)
+
+  localStorage.setItem('tarefas', JSON.stringify(tarefas))
 
   input.value = " "
 }
