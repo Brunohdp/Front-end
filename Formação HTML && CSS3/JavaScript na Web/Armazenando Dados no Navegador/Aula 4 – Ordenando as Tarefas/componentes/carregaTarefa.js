2b9113156d6e61b3ed79a261faa35b18b76b0844
Bruno Henrique de Paula <78300872+Orecruta@users.noreply.github.com>
2021-06-21 13:51:42 -0300

    Organizando as tarefas
    Fizemos um código para organizar as tarefas por datas de execução, deixando tudo mais organizado.

--- Formação HTML && CSS3/JavaScript na Web/Armazenando Dados no Navegador/Aula 4 – Ordenando as Tarefas/componentes/carregaTarefa.js
+++ Formação HTML && CSS3/JavaScript na Web/Armazenando Dados no Navegador/Aula 4 – Ordenando as Tarefas/componentes/carregaTarefa.js
@@ -1,12 +1,17 @@
-import { Tarefa } from "./criaTarefa.js"
+import { criaData } from "./criaData.js"
 
 export const carregaTarefa = () => {
   const lista = document.querySelector('[data-list]')
 
-  const tarefasCadastradas = JSON.parse(localStorage.getItem('trefas'))||[]
+  const tarefasCadastradas = JSON.parse(localStorage.getItem('trefas')) || []
 
   lista.innerHTML = " "
-  tarefasCadastradas.forEach((tarefa) => {
-    lista.appendChild(Tarefa(tarefa))
-  })
+  tarefasCadastradas.forEach((tarefa => {
+    const dia = moment(tarefa.dataFormatada, 'DD/MM/YYYY')
+    const diff = data.diff(dia)
+
+    if (diff == 0) {
+      lista.appendChild(criaData(dia))
+    }
+  }))
 }
---
+++ Formação HTML && CSS3/JavaScript na Web/Armazenando Dados no Navegador/Aula 4 – Ordenando as Tarefas/componentes/criaData.js
@@ -0,0 +1,20 @@
+import { Tarefa } from "./criaTarefa.js"
+
+export const criaData = (data) => {
+  const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+  const dataTopo = document.createElement('li')
+  const conteudo = `<p class="content-data">${data.format('DD/MM/YYYY')}</p>`
+
+  dataTopo.innerHTML = conteudo
+
+  tarefas.forEach((tarefas => {
+    const dia = moment(tarefa.dataFormatada, 'DD/MM/YYYY')
+
+    const diff = data.diff(dia)
+    if (diff == 0) {
+      dataTopo.appendChild(Tarefa(Tarefa))
+    }
+  }))
+
+  return dataTopo
+}
