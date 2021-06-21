f2e128e4c25f790991569b51791ee0463ce28c31
Bruno Henrique de Paula <78300872+Orecruta@users.noreply.github.com>
2021-06-21 13:22:30 -0300

    Deixando esticamente como do professor

--- Formação HTML && CSS3/JavaScript na Web/Armazenando Dados no Navegador/Aula 3 – Renderizando a Lista/componentes/criaTarefa.js
+++ Formação HTML && CSS3/JavaScript na Web/Armazenando Dados no Navegador/Aula 3 – Renderizando a Lista/componentes/criaTarefa.js
@@ -1,24 +1,22 @@
+import { carregaTarefa } from './carregaTarefa.js'
 import BotaoConclui from './concluiTarefa.js'
 import BotaoDeleta from './deletaTarefa.js'
-import { carregaTarefa } from './carregaTarefa.js'
+
 
 export const handleNovoItem = (evento) => {
-
   evento.preventDefault()
-  
   const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
   const input = document.querySelector('[data-form-input]')
   const valor = input.value
-  
+
   const calendario = document.querySelector('[data-form-date]')
   const data = moment(calendario.value)
+
   const dataFormatada = data.format('DD/MM/YYYY')
-  
+
   const dados = {
-    
     valor,
     dataFormatada
-
   }
 
   const tarefasAtualizadas = [...tarefas, dados]
@@ -28,6 +26,7 @@ export const handleNovoItem = (evento) => {
   input.value = " "
 
   carregaTarefa()
+
 }
 
 export const Tarefa = ({ valor, dataFormatada }) => {
