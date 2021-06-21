f74cf3187a3a7207b2c1c2be1af40293f6d7765a
Bruno Henrique de Paula <78300872+Orecruta@users.noreply.github.com>
2021-06-21 10:48:21 -0300

    Iniciando a aula 3 do curso de JS Web
    Aula 3 – Renderizando a Lista
    Ambiente criado para o início da aula

---
+++ Formação HTML && CSS3/JavaScript na Web/Armazenando Dados no Navegador/Aula 3 – Renderizando a Lista/main.js
@@ -0,0 +1,5 @@
+import { handleNovoItem } from "./componentes/criaTarefa.js"
+
+const novaTarefa = document.querySelector('[data-form-button]')
+
+novaTarefa.addEventListener('click', handleNovoItem)
