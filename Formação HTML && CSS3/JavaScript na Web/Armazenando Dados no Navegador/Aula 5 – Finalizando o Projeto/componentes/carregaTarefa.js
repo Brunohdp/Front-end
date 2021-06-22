628771c15259e1bd7070eea3072e423dfd2e4a91
Bruno Henrique de Paula <78300872+Orecruta@users.noreply.github.com>
2021-06-22 08:53:22 -0300

    Iniciando a aula 5 do curso de JS WEB
    Aula 5 – Finalizando o Projeto
    Ambiente pronto

---
+++ Formação HTML && CSS3/JavaScript na Web/Armazenando Dados no Navegador/Aula 5 – Finalizando o Projeto/componentes/carregaTarefa.js
@@ -0,0 +1,14 @@
+import { ordenaDatas, removeDatasRepetidas } from "../service/data.js"
+import { criaData } from "./criaData.js"
+export const carregaTarefa = () => {
+  const lista = document.querySelector('[data-list]')
+
+  const tarefasCadastradas = JSON.parse(localStorage.getItem('trefas')) || []
+
+  lista.innerHTML = " "
+  const datasUnicas = removeDatasRepetidas(tarefasCadastradas)
+  ordenaDatas(datasUnicas)
+  datasUnicas.forEach((dia) => {
+    lista.appendChild(criaData(dia))
+  })
+}
