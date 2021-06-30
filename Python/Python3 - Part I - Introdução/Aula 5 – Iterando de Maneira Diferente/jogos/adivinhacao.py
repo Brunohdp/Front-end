4bf7b9e45d2f79f88c17264b17f8b3cdddd45a43
Bruno Henrique de Paula <78300872+Orecruta@users.noreply.github.com>
2021-06-29 21:18:29 -0300

    Encerrando interação e o loop
    Aprendi a cancelar o loop com break e cancelar uma ação, mas continuar o loop com o continue

--- Python/Python3 - Part I - Introdução/Aula 5 – Iterando de Maneira Diferente/jogos/adivinhacao.py
+++ Python/Python3 - Part I - Introdução/Aula 5 – Iterando de Maneira Diferente/jogos/adivinhacao.py
@@ -6,8 +6,12 @@ numero_secreto = 42
 total_de_tentativas = int(input("Quantas vezes deseja tentar?\nR: "))
 
 for rodada in range(1, total_de_tentativas + 1):
-    print("Tentativa {} de {}", format(rodada, total_de_tentativas))
-    chute = int(input("Digite um número!\nR: "))
+    print('Tentativa {} de {}', format(rodada, str(total_de_tentativas)))
+    chute = int(input("Digite um número entre 1 e 100!\nR: "))
+
+    if (chute < 1 or chute > 100):
+        print("\nVocê deve digitar um número entre 1 e 100!\n")
+        continue
 
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
@@ -15,6 +19,7 @@ for rodada in range(1, total_de_tentativas + 1):
 
     if (acertou):
         print('Parabéns, você acertou! O número secreto era:', numero_secreto)
+        break
     else:
         if (maior):
             print('Oh não, você errou! O número secreto está mais para baixo do que seu chute')
