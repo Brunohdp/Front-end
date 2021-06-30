590a254ac301e938c9ce4e36c5f7d224c2d4f671
Bruno Henrique de Paula <78300872+Orecruta@users.noreply.github.com>
2021-06-30 15:41:28 -0300

    Game loop
    Fizemos o loop do jogo e aprendemos sobre operador lógico and

--- Python/Python3 - Part II - Avançando/Aula 1 – Preparando o Jogo da Forca/jogos/forca.py
+++ Python/Python3 - Part II - Avançando/Aula 1 – Preparando o Jogo da Forca/jogos/forca.py
@@ -3,6 +3,15 @@ def jogar():
     print("* Bem vindo ao jogo da forca! *")
     print("*" * 37)
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+        print("jogando...")
+    
+
     print("fim do jogo")
 
 if (__name__ == "__main__"):
