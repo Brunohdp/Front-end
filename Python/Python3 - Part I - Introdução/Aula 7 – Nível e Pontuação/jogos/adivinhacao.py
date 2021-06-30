1e05a5988e3630f9ba29db380c3424f1271d2812
Bruno Henrique de Paula <78300872+Orecruta@users.noreply.github.com>
2021-06-30 12:52:16 -0300

    Adicionando níveis
    Adicionamos escolha de níveis de dificuldade para o jogador escolher

--- Python/Python3 - Part I - Introdução/Aula 7 – Nível e Pontuação/jogos/adivinhacao.py
+++ Python/Python3 - Part I - Introdução/Aula 7 – Nível e Pontuação/jogos/adivinhacao.py
@@ -1,15 +1,35 @@
 from random import randint
 
 print("*" * 37)
-print("| Bem vindo ao jogo de adivinhação! |")
+print("* Bem vindo ao jogo de adivinhação! *")
 print("*" * 37)
 
 numero_secreto = randint(1, 100)
-total_de_tentativas = int(input("Quantas vezes deseja tentar?\nR: "))
+total_de_tentativas = 0
+
+print("-" * 34)
+print("| Escolha o nível de dificuldade |")
+print("-" * 34)
+
+nivel = int(input("| (1) Fácil   |\n| (2) Médio   |\n| (3) Difícil |\n| R: "))
+print("-" * 15)
+
+if (nivel == 1):
+    total_de_tentativas = 20
+elif (nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1):
-    print(f'Tentativa {rodada:d} de {total_de_tentativas:d}')
-    chute = int(input("Digite um número entre 1 e 100!\nR: "))
+    print("*" * 22)
+    print(f'* Tentativa {rodada:02} de {total_de_tentativas:2} *')
+    print("*" * 22)
+    print("-" * 35)
+    print("| Digite um número entre 1 e 100: |")
+    print("-" * 35)
+    chute = int(input("| R: ", end="|"))
+    print("-" * 35)
 
     if (chute < 1 or chute > 100):
         print("\nVocê deve digitar um número entre 1 e 100!\n")
