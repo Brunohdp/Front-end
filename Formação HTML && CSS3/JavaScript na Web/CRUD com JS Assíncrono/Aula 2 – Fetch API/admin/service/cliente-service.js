dda8bb34b2115c427203974d47b7198942b41875
Bruno Henrique de Paula <78300872+Orecruta@users.noreply.github.com>
2021-07-21 15:02:07 -0300

    Reescrevendo com fetch API
    Aprendemos como substituir vários códigos usando ele

--- Formação HTML && CSS3/JavaScript na Web/CRUD com JS Assíncrono/Aula 2 – Fetch API/admin/service/cliente-service.js
+++ Formação HTML && CSS3/JavaScript na Web/CRUD com JS Assíncrono/Aula 2 – Fetch API/admin/service/cliente-service.js
@@ -16,21 +16,10 @@ const criaNovaLinha = (nome, email) => {
 const tabela = document.querySelector('[data-tabela]')
 
 const listaClientes = () => {
-  const promise = new Promise((resolve, reject) => {
-    const http = new XMLHttpRequest()
-    http.open('GET', 'http://localhost:3000/profile')
-
-    http.onload = () => {
-      if (http.status >= 400) {
-        reject(JSON.parse(http.response))
-      } else {
-        resolve(JSON.parse(http.response))
-      }
-    }
-    http.send()
+  return fetch(`http://localhost:3000/profile`)
+  .then(Responsta => {
+    return Responsta.json()
   })
-  console.log(promise)
-  return promise
 }
 
 listaClientes()
