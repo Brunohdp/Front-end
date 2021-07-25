ebab17d03a6d1ae1c9290bbc2bde80b0cbdd6ab6
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-07-25 13:16:10 -0300

    Conectando API com botão
    Conectamos o código de deletar cliente com o botão "excluir"

--- Formação HTML && CSS3/JavaScript na Web/CRUD com JS Assíncrono/Aula 3 – Criar e Remover Clientes/admin/service/cliente-service.js
+++ Formação HTML && CSS3/JavaScript na Web/CRUD com JS Assíncrono/Aula 3 – Criar e Remover Clientes/admin/service/cliente-service.js
@@ -1,29 +1,30 @@
 const listaClientes = () => {
   return fetch(`http://localhost:3000/profile`)
-  .then(resposta => {
-    return resposta.json()
-  })
+    .then(resposta => {
+      return resposta.json()
+    })
 }
 
 const criaCliente = (nome, email) => {
   return fetch(`http://localhost:3000/profile`, {
-    method: 'POST',
-    headers: {
-      'Content-Type' : 'application/json'
-    },
-    body: JSON.stringify({
-      nome: nome,
-      email: email
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        nome: nome,
+        email: email
+      })
     })
-  })
-  .then(resposta => {
-    return resposta.body
-  })
+    .then(resposta => {
+      return resposta.body
+    })
 }
 
 const removeCliente = (id) => {
-  return fetch(`http://localhost:3000/profile/${id}`)
-  method: 'DELETE'
+  return fetch(`http://localhost:3000/profile/${id}`, {
+    method: 'DELETE'
+  })
 }
 
 export const clienteService = {
