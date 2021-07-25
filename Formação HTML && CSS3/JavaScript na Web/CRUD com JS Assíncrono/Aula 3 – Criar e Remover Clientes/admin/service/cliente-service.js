a95e27557d0c1b1aa6a501bd5e937409c8665ef7
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-07-25 12:55:56 -0300

    Deletando um cliente
    Fizemos o código de deletar um cliente

--- Formação HTML && CSS3/JavaScript na Web/CRUD com JS Assíncrono/Aula 3 – Criar e Remover Clientes/admin/service/cliente-service.js
+++ Formação HTML && CSS3/JavaScript na Web/CRUD com JS Assíncrono/Aula 3 – Criar e Remover Clientes/admin/service/cliente-service.js
@@ -21,7 +21,13 @@ const criaCliente = (nome, email) => {
   })
 }
 
+const removeCliente = (id) => {
+  return fetch(`http://localhost:3000/profile/${id}`)
+  method: 'DELETE'
+}
+
 export const clienteService = {
   listaClientes,
-  criaCliente
+  criaCliente,
+  removeCliente
 }
