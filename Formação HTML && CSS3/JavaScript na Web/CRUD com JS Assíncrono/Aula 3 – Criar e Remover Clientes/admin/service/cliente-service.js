30119f69f172c00c3ecdb918c9924e49584f62b2
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-07-25 12:33:49 -0300

    Criando um cliente
    Aprendemos como criar um cliente e mandar para o servidor que irá exibir exibir na aba clientes

--- Formação HTML && CSS3/JavaScript na Web/CRUD com JS Assíncrono/Aula 3 – Criar e Remover Clientes/admin/service/cliente-service.js
+++ Formação HTML && CSS3/JavaScript na Web/CRUD com JS Assíncrono/Aula 3 – Criar e Remover Clientes/admin/service/cliente-service.js
@@ -1,10 +1,27 @@
 const listaClientes = () => {
   return fetch(`http://localhost:3000/profile`)
-  .then(responsta => {
-    return responsta.json()
+  .then(resposta => {
+    return resposta.json()
+  })
+}
+
+const criaCliente = (nome, email) => {
+  return fetch(`http://localhost:3000/profile`, {
+    method: 'POST',
+    headers: {
+      'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify({
+      nome: nome,
+      email: email
+    })
+  })
+  .then(resposta => {
+    return resposta.body
   })
 }
 
 export const clienteService = {
-  listaClientes
+  listaClientes,
+  criaCliente
 }
