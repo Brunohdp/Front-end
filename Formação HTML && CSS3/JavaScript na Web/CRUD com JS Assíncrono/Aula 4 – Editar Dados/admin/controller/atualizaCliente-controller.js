00102e6b71cf8923c3e26b6691b08a0e77126150
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-07-25 13:52:49 -0300

    Exibindo dados para edição
    Aprendemos a como exibir os dados editados e mandá-los para o servidor

--- Formação HTML && CSS3/JavaScript na Web/CRUD com JS Assíncrono/Aula 4 – Editar Dados/admin/controller/atualizaCliente-controller.js
+++ Formação HTML && CSS3/JavaScript na Web/CRUD com JS Assíncrono/Aula 4 – Editar Dados/admin/controller/atualizaCliente-controller.js
@@ -8,7 +8,18 @@ const inputNome = document.querySelector('[data-nome]')
 const inputEmail = document.querySelector('[data-email]')
 
 clienteService.detalhaCliente(id)
-.then(dados => {
-  inputNome.value = dados.nome
-  inputEmail.value = dados.email
+  .then(dados => {
+    inputNome.value = dados.nome
+    inputEmail.value = dados.email
+  })
+
+const formulario = document.querySelector('[data-form]')
+
+formulario.addEventListener('submit', (evento) => {
+  evento.preventDefault()
+
+  clienteService.atualizaCliente(id, inputNome.value, inputEmail.value)
+  .then(() => {
+    window.location.href = "../telas/edicao_concluida.html"
+  })
 })
--- Formação HTML && CSS3/JavaScript na Web/CRUD com JS Assíncrono/Aula 4 – Editar Dados/admin/service/cliente-service.js
+++ Formação HTML && CSS3/JavaScript na Web/CRUD com JS Assíncrono/Aula 4 – Editar Dados/admin/service/cliente-service.js
@@ -3,10 +3,10 @@ const listaClientes = () => {
     .then(resposta => {
       return resposta.json()
     })
-  }
-  
-  const criaCliente = (nome, email) => {
-    return fetch(`http://localhost:3000/profile`, {
+}
+
+const criaCliente = (nome, email) => {
+  return fetch(`http://localhost:3000/profile`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -34,9 +34,26 @@ const detalhaCliente = (id) => {
     })
 }
 
+const atualizaCliente = (id, nome, email) => {
+  return fetch(`http://localhost:3000/profile/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        nome: nome,
+        email: email
+      })
+    })
+    .then(resposta => {
+      return resposta.json()
+    })
+}
+
 export const clienteService = {
   listaClientes,
   criaCliente,
   removeCliente,
-  detalhaCliente
+  detalhaCliente,
+  atualizaCliente
 }
