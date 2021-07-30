fd042e3943f1484c1f6f8daf1f4e111f74a7f720
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-07-30 15:29:32 -0300

    Mostrando mensagem de erro
    Aprendemos como adicionar e remover classes à um elemento para poder exibir mensagens de erros personalizadas, além de escrever uma mensagem diferente para cada tipo de erro possível

--- Formação HTML && CSS3/JavaScript na Web/Validação de Formulários e HTML5/Aula 3 – Mensagens Customizadas/js/validacao.js
+++ Formação HTML && CSS3/JavaScript na Web/Validação de Formulários e HTML5/Aula 3 – Mensagens Customizadas/js/validacao.js
@@ -4,6 +4,30 @@ export function valida(input) {
   if (validadores[tipoDeInput]) {
     validadores[tipoDeInput](input)
   }
+
+  if (input.validity.valid) {
+    input.parentElement.classList.remove('input-container--invalido')
+  } else {
+    input.parentElement.classList.add('input-container--invalido')
+  }
+}
+
+const mensagensDeErro = {
+  nome: {
+    valueMissing: 'O campo "nome" não pode estar vazio.'
+  },
+  email: {
+    valueMissing: 'O campo "email" não pode estar vazio.',
+    typeMismatch: 'O e-mail digitado não é válido'
+  },
+  senha: {
+    valueMissing: 'O campo "senha" não pode estar vazio.',
+    patternMismatch: 'A senha deve conter entre 6 à 12 caracteres, uma letra maiúscula, um número e não deve conter símbolos.'
+  },
+  dataNascimento: {
+    valueMissing: 'O campo "data de Nascimento" não pode estar vazio.',
+    customError: 'Você deve ser maior de 18 anos para se cadastrar'
+  }
 }
 
 const validadores = {
