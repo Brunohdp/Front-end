6914ceb0da214575c40afee61649b1a1b7463459
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-08-18 09:35:51 -0300

    Iniciando aula 3 do curso de Python
    Aula 3 – Conhecendo e Trabalhando com Listas
    Ambiente ok

---
+++ Python/Python3 - Part II - Avançando/Aula 3 – Conhecendo e Trabalhando com Listas/jogos/forca.py
@@ -0,0 +1,18 @@
+def jogar():
+    print("*" * 37)
+    print("* Bem vindo ao jogo da forca! *")
+    print("*" * 37)
+
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+        print("jogando...")
+    
+
+    print("fim do jogo")
+
+if (__name__ == "__main__"):
+    jogar()
