46154a313ba13b3f7c1f2fef7bcd4e135b085130
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-08-22 12:55:59 -0300

    Organizando o código em funções
    aprendi a organizar os códigos em funções e porque fazer isso

--- Python/Python3 - Part II - Avançando/Aula 7 – Melhorando o Código e Apresentação/jogos/forca.py
+++ Python/Python3 - Part II - Avançando/Aula 7 – Melhorando o Código e Apresentação/jogos/forca.py
@@ -1,23 +1,9 @@
 import random
 
 def jogar():
-    print("*" * 37)
-    print("* Bem vindo ao jogo da forca! *")
-    print("*" * 37)
-
-    arquivo = open("palavras.txt", "r")
-    palavras = []
-
-    for linha in arquivo:
-        linha = linha.strip()
-        palavras.append(linha)
-
-    arquivo.close()
-
-    numero = random.randrange(0, len(palavras))
-    palavra_secreta = palavras[numero].upper()
-
-    letras_acertadas = ["_" for letra in palavra_secreta]
+    imprime_mensagem_abertura()
+    palavra_secreta = carrega_palavra_secreta()
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
 
     enforcou = False
     acertou = False
@@ -49,6 +35,31 @@ def jogar():
     else:
         print(f'Ah não, você perdeu :(. A palavra secreta era {palavra_secreta}. Quem sabe na próxima ;)')
     print("fim do jogo")
+
+
+def imprime_mensagem_abertura():
+    print("*" * 37)
+    print("* Bem vindo ao jogo da forca! *")
+    print("*" * 37)
+
+
+def carrega_palavra_secreta():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    return palavra_secreta
+
+
+def inicializa_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
 
 
 if (__name__ == "__main__"):
