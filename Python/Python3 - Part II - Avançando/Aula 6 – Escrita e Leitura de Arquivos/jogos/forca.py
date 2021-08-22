0d664b464e95eb3d8ec83b0c60173ae6cfb0926d
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-08-22 11:42:32 -0300

    Escolhendo uma palavra
    Aprendemos a utilizar a biblioteca random para aleatorizar uma palavra da lista e utilizar ela no código.

--- Python/Python3 - Part II - Avançando/Aula 6 – Escrita e Leitura de Arquivos/jogos/forca.py
+++ Python/Python3 - Part II - Avançando/Aula 6 – Escrita e Leitura de Arquivos/jogos/forca.py
@@ -1,9 +1,22 @@
+import random
+
 def jogar():
     print("*" * 37)
     print("* Bem vindo ao jogo da forca! *")
     print("*" * 37)
 
-    palavra_secreta = "banana".upper()
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
     letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
