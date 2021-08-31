6dde3b93e11c44a9bbffb3d792dc3b46be99e3ca
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-08-31 14:44:37 -0300

    Construtor
    Aprendemos o que são e como utilizar construtores

--- Python/Orientação a objetos/Introdução a orientação a objetos/Aula 2 – Classes e Objetos/conta.py
+++ Python/Orientação a objetos/Introdução a orientação a objetos/Aula 2 – Classes e Objetos/conta.py
@@ -2,4 +2,8 @@
 
 class Conta:
 
-    pass
+    def __init__(self, numero, titular, saldo, limite):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
