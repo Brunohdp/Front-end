f05fdb9853a426732a9b6a84e3aa8aaecf3d6ad6
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-09-01 11:32:50 -0300

    Getters e Setters
    Aprendemos sobre a nomenclatura get e set nos métodos

--- Python/Orientação a objetos/Introdução a orientação a objetos/Aula 5 – Usando Propriedades/conta.py
+++ Python/Orientação a objetos/Introdução a orientação a objetos/Aula 5 – Usando Propriedades/conta.py
@@ -20,3 +20,16 @@ class Conta:
     def transfere(self, destino, valor):
         self.saca(valor)
         destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_limite(self):
+        return self.__limite
+
+    def set_limite(self, limite):
+        self.__limite = limite
+
