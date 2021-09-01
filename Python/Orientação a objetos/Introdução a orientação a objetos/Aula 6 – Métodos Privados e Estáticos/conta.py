0c7b39165938bfc07970bf25f3424242f4bee496
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-09-01 13:30:01 -0300

    Métodos da Classe
    Aprendemos a usar métodos da classe a partir da transformação deles em estáticos

--- Python/Orientação a objetos/Introdução a orientação a objetos/Aula 6 – Métodos Privados e Estáticos/conta.py
+++ Python/Orientação a objetos/Introdução a orientação a objetos/Aula 6 – Métodos Privados e Estáticos/conta.py
@@ -44,3 +44,10 @@ class Conta:
     def limite(self, limite):
         self.__limite = limite
 
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
