8e85552d9a8ea0db579689ec9447637181130015
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-09-01 10:29:09 -0300

    Atributos privados
    Aprendemos o que são os atributos privados, como transformar um atributo em privado, o que o python faz quando isso acontece e o que precisamos entender quando eles são privados

--- Python/Orientação a objetos/Introdução a orientação a objetos/Aula 4 – Encapsulamento/conta.py
+++ Python/Orientação a objetos/Introdução a orientação a objetos/Aula 4 – Encapsulamento/conta.py
@@ -3,16 +3,16 @@
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(f'Saldo de {self.saldo} do titular {self.titular}')
+        print(f'Saldo de {self.__saldo} do titular {self.__titular}')
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
