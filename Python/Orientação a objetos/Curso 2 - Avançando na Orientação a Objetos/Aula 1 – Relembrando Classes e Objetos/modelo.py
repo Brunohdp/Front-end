a90d77ebd8c29f5ed667f0e6df8a360132c27cc6
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-09-03 10:09:22 -0300

    Adicionando atributos e métodos
    Adicionamos atributos e métodos no código. Nada novo por enquanto

--- Python/Orientação a objetos/Curso 2 - Avançando na Orientação a Objetos/Aula 1 – Relembrando Classes e Objetos/modelo.py
+++ Python/Orientação a objetos/Curso 2 - Avançando na Orientação a Objetos/Aula 1 – Relembrando Classes e Objetos/modelo.py
@@ -1,18 +1,31 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
 
 class Series:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
+
 
 vingadores = Filme('Vingadores - guerra infinita', 2018, 160)
-print(f'Nome: {vingadores.nome}.\nAno: {vingadores.ano}.\nDuração: {vingadores.duracao}.')
+print(f'Nome: {vingadores.nome}. Ano: {vingadores.ano}. '
+      f'Duração: {vingadores.duracao}. Likes: {vingadores.likes}')
 
 
 atlanta = Series('Atlanta', 2018, 2)
-print(f'Nome: {atlanta.nome}.\nAno: {atlanta.ano}.\nTemporadas: {atlanta.temporadas}.')
+atlanta.dar_like()
+atlanta.dar_like()
+print(f'Nome: {atlanta.nome}. Ano: {atlanta.ano}. '
+      f'Temporadas: {atlanta.temporadas}. Likes: {atlanta.likes}')
