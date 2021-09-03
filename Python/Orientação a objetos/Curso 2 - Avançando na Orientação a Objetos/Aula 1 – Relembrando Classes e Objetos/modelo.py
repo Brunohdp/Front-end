f04adeb6168849122604e10a5fd689555f640a2c
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-09-03 10:16:54 -0300

    Encapsulando comportamento
    Privamos atributos e colocamos getters e setters

--- Python/Orientação a objetos/Curso 2 - Avançando na Orientação a Objetos/Aula 1 – Relembrando Classes e Objetos/modelo.py
+++ Python/Orientação a objetos/Curso 2 - Avançando na Orientação a Objetos/Aula 1 – Relembrando Classes e Objetos/modelo.py
@@ -1,22 +1,46 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome.title()
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.likes = 0
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    @property
+    def nome(self):
+        return self.__nome
 
     def dar_like(self):
-        self.likes += 1
+        self.__likes += 1
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
 
 class Series:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome.title()
+        self.__nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
-        self.likes = 0
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    @property
+    def nome(self):
+        return self.__nome
 
     def dar_like(self):
-        self.likes += 1
+        self.__likes += 1
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
 
 
 vingadores = Filme('Vingadores - guerra infinita', 2018, 160)
