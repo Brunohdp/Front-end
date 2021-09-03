3171c095b3cc206df35f4fd8beabae7cd9c18a71
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-09-03 10:56:11 -0300

    Removendo duplicação
    Removemos a duplicação usando herança e aprendemos um pouco sobre dificuldades de se usar atributos privados quando se usa método de herança

--- Python/Orientação a objetos/Curso 2 - Avançando na Orientação a Objetos/Aula 2 – Removendo Duplicação com Herança/modelo.py
+++ Python/Orientação a objetos/Curso 2 - Avançando na Orientação a Objetos/Aula 2 – Removendo Duplicação com Herança/modelo.py
@@ -1,46 +1,40 @@
-class Filme:
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    @property
+    def nome(self):
+        return self._nome
+
+    def dar_like(self):
+        self._likes += 1
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.__likes = 0
+        self._likes = 0
 
-    @property
-    def likes(self):
-        return self.__likes
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    def dar_like(self):
-        self.__likes += 1
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-class Series:
+class Series(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
-        self.__likes = 0
+        self._likes = 0
 
-    @property
-    def likes(self):
-        return self.__likes
 
-    @property
-    def nome(self):
-        return self.__nome
-
-    def dar_like(self):
-        self.__likes += 1
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
 
 
 vingadores = Filme('Vingadores - guerra infinita', 2018, 160)
