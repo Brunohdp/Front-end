23f9196b33c647aab02f16a69c1e3709f43bec27
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-09-04 16:28:33 -0300

    Explicando a herança
    Aprendemos um pouco mais sobre herança

--- Python/Orientação a objetos/Curso 2 - Avançando na Orientação a Objetos/Aula 2 – Removendo Duplicação com Herança/modelo.py
+++ Python/Orientação a objetos/Curso 2 - Avançando na Orientação a Objetos/Aula 2 – Removendo Duplicação com Herança/modelo.py
@@ -30,17 +30,14 @@ class Series(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
-        
+
 
 
 
 vingadores = Filme('Vingadores - guerra infinita', 2018, 160)
-print(f'Nome: {vingadores.nome}. Ano: {vingadores.ano}. '
-      f'Duração: {vingadores.duracao}. Likes: {vingadores.likes}')
-
+print(f'{vingadores.nome} - {vingadores.duracao}: {vingadores.likes}')
 
 atlanta = Series('Atlanta', 2018, 2)
 atlanta.dar_like()
 atlanta.dar_like()
-print(f'Nome: {atlanta.nome}. Ano: {atlanta.ano}. '
-      f'Temporadas: {atlanta.temporadas}. Likes: {atlanta.likes}')
+print(f'{atlanta.nome} - {atlanta.temporadas}: {atlanta.likes}')
