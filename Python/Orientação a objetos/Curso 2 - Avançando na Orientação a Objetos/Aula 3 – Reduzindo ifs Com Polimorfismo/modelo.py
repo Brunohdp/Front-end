37e9c0d9e54c74dd0f06cb25959c93f289f73de9
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-09-05 12:41:25 -0300

    Representação textual de objetos
    Aprendemos como criar um método interno que possibilita representar um objeto de maneira textual apenas pedindo para que aquele objeto seja impresso, ao invés de criar métodos do tipo imprime() com polimorfismo ou vários ifs para fazer alterações e imprimir o que queremos

--- Python/Orientação a objetos/Curso 2 - Avançando na Orientação a Objetos/Aula 3 – Reduzindo ifs Com Polimorfismo/modelo.py
+++ Python/Orientação a objetos/Curso 2 - Avançando na Orientação a Objetos/Aula 3 – Reduzindo ifs Com Polimorfismo/modelo.py
@@ -19,8 +19,8 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self._likes}')
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self._likes}'
 
 
 
@@ -29,8 +29,8 @@ class Filme(Programa):
         super().__init__(nome, ano)
         self.duracao = duracao
 
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes}')
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes}'
 
 
 class Series(Programa):
@@ -38,8 +38,8 @@ class Series(Programa):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self.temporadas} temp. - {self._likes}')
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.temporadas} temp. - {self._likes}'
 
 
 
@@ -53,4 +53,4 @@ atlanta.dar_like()
 filmes_e_series = [vingadores, atlanta]
 
 for programa in filmes_e_series:
-    programa.imprime()
+    print(programa)
