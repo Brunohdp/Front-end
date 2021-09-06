17b3b2fec5b5356b1d250556c279ac0a977f3960
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-09-06 07:58:59 -0300

    Se anda como um pato...
    Aprendemos o significado, da onde vem, suas vantagens e quando utilizar o duck typing.

--- Python/Orientação a objetos/Curso 2 - Avançando na Orientação a Objetos/Aula 5 – Duck Typing e um Modelo de Dados/modelo.py
+++ Python/Orientação a objetos/Curso 2 - Avançando na Orientação a Objetos/Aula 5 – Duck Typing e um Modelo de Dados/modelo.py
@@ -47,6 +47,9 @@ class Playlist:
         self.nome = nome
         self._programas = programas
 
+    def __getitem__(self, item):
+        return self._programas[item]
+
     @property
     def listagem(self):
         return self._programas
@@ -80,5 +83,5 @@ playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
 print(f'Tamanho da minha playlist: {len(playlist_fim_de_semana.listagem)}')
 
-for programa in playlist_fim_de_semana.listagem:
+for programa in playlist_fim_de_semana:
     print(programa)
