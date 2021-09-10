71ecdd9413fddc601acaac795655ce5b261699d9
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-09-10 11:33:20 -0300

    O método find
    Aprendemos a usar o método find para fazer o fatiamento e outras técnicas que o python nos permite

--- Python/String em python - Extraindo informações de uma URL/Aula 2 – Utilizando Métodos de Strings/main.py
+++ Python/String em python - Extraindo informações de uma URL/Aula 2 – Utilizando Métodos de Strings/main.py
@@ -1,8 +1,9 @@
 url = "bytebank.com/cambio?moedaOrigem=real"
 print(url)
 
-url_base = url[0:19]
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
 print(url_base)
 
-url_parametros = url[20:36]
+url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
