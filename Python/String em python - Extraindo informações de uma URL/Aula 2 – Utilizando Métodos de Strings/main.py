75653731c50e5f8ce0460652d1169515aa0b71de
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-09-10 12:09:10 -0300

    O método len
    Aprendemos como utilizar o len com fatiamento

--- Python/String em python - Extraindo informações de uma URL/Aula 2 – Utilizando Métodos de Strings/main.py
+++ Python/String em python - Extraindo informações de uma URL/Aula 2 – Utilizando Métodos de Strings/main.py
@@ -1,9 +1,13 @@
-url = "bytebank.com/cambio?moedaOrigem=real"
-print(url)
+url = "bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
 
 indice_interrogacao = url.find('?')
 url_base = url[:indice_interrogacao]
-print(url_base)
 
 url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
+
+parametro_busca = 'moedaOrigem'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+valor = url_parametros[indice_valor:]
+print(valor)
