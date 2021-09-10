3b32ed4bdbc5a8f5d74b2de9e611e88db6d825c3
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-09-10 11:08:57 -0300

    Fatiamento de Strings
    Aprendemos a usar o fatiamento para determinar o que queremos pegar da nossa url

--- Python/String em python - Extraindo informações de uma URL/Aula 1 – Introdução e Fatiamento de Strings/main.py
+++ Python/String em python - Extraindo informações de uma URL/Aula 1 – Introdução e Fatiamento de Strings/main.py
@@ -1,2 +1,8 @@
-url = "1.2.1.	https://bytebank.com/cambio?moedaOrigem=real&moedaDestino&quantidade=100"
+url = "bytebank.com/cambio?moedaOrigem=real"
 print(url)
+
+url_base = url[0:19]
+print(url_base)
+
+url_parametros = url[20:36]
+print(url_parametros)
