ec406f8d88264f73725c233887f2cf0b8176a6ff
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-09-22 14:26:53 -0300

    Validando nossa URL com RegEx
    Fizemos um código para validar a url com regex

--- Python/String em python - Extraindo informações de uma URL/Aula 4 – Expressões Regulares/extrator_url.py
+++ Python/String em python - Extraindo informações de uma URL/Aula 4 – Expressões Regulares/extrator_url.py
@@ -1,3 +1,5 @@
+import re
+
 class ExtratorURL:
     def __init__(self, url):
         self.url = self.sanitiza_url(url)
@@ -12,6 +14,12 @@ class ExtratorURL:
     def valida_url(self):
         if not self.url:
             raise ValueError("A URL está vazia")
+
+        padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
+        match = padrao_url.match(url)
+
+        if not match:
+            raise ValueError("A URL não é válida.")
 
     def get_url_base(self):
         indice_interrogacao = self.url.find('?')
@@ -34,6 +42,7 @@ class ExtratorURL:
         return valor
 
 
-extrator_url = ExtratorURL(None)
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+extrator_url = ExtratorURL(url)
 valor_quantidade = extrator_url.get_valor_parametro("quantidade")
 print(valor_quantidade)
