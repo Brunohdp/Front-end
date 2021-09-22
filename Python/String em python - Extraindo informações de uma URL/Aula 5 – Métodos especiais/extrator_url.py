9da41a3395066ca78471ca7fa253983b950c88bd
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-09-22 14:36:52 -0300

    O método __len__(self)
    Aprendemos para que serve, como e quando usar

--- Python/String em python - Extraindo informações de uma URL/Aula 5 – Métodos especiais/extrator_url.py
+++ Python/String em python - Extraindo informações de uma URL/Aula 5 – Métodos especiais/extrator_url.py
@@ -41,8 +41,13 @@ class ExtratorURL:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
         return valor
 
+    def __len__(self):
+        return len(self.url)
+
 
 url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 extrator_url = ExtratorURL(url)
+print("o tamanho da URL: ", len(extrator_url))
+
 valor_quantidade = extrator_url.get_valor_parametro("quantidade")
 print(valor_quantidade)
