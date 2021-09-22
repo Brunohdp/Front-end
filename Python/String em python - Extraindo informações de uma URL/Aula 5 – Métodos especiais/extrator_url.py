2093fddb98d839ce08259f879fc7894d7e5844a3
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-09-22 14:56:20 -0300

    Igualdade e identidade
    Aprendemos a diferença entre os dois

--- Python/String em python - Extraindo informações de uma URL/Aula 5 – Métodos especiais/extrator_url.py
+++ Python/String em python - Extraindo informações de uma URL/Aula 5 – Métodos especiais/extrator_url.py
@@ -47,11 +47,12 @@ class ExtratorURL:
     def __str__(self):
         return "URL: " + self.url + "\n" + "Parâmetros: " + self.get_url_parametros() + "\n" + "URL Base: " + self.get_url_base()
 
+    def __eq__(self, other):
+        return self.url == other.url
+
 
 url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 extrator_url = ExtratorURL(url)
-print("o tamanho da URL: ", len(extrator_url))
-print(extrator_url)
+extrator_url_2 = ExtratorURL(url)
 
-valor_quantidade = extrator_url.get_valor_parametro("quantidade")
-print(valor_quantidade)
+print(extrator_url == extrator_url_2)
