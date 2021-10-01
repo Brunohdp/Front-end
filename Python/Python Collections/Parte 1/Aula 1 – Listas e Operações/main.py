3ff583cd7c9c4d4d75619223a847f4ffb1e90986
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-10-01 10:24:32 -0300

    Mais operações em listas e list comprehension
    Aprendemos sobre filtros e muito mais

--- Python/Python Collections/Parte 1/Aula 1 – Listas e Operações/main.py
+++ Python/Python Collections/Parte 1/Aula 1 – Listas e Operações/main.py
@@ -1,7 +1,33 @@
 idades = [39, 30, 27, 18]
-print(idades)
-idades.append(15)
-print(idades)
+# print(idades)
+# idades.append(15)
+# print(idades)
+#
+# for idade in idades:
+#     print(idade)
+#
+# if 15 in idades:
+#     idades.remove(15)
+#
+# # idades.append([55, 33])
+# # print(idades)
+#
+# for elemento in idades:
+#     print(f'Recebendo o elemento: {elemento}')
+#
+# idades.extend([55, 33])
+# print(idades)
+#
+# for cada in idades:
+#     print(f'Recebendo o elemento: {cada}')
 
+idades_no_ano_que_vem = []
 for idade in idades:
-    print(idade)
+    idades_no_ano_que_vem.append(idade+1)
+print(idades_no_ano_que_vem)
+
+idades_no_ano_que_vem = [(idade+1) for idade in idades]
+print(idades_no_ano_que_vem)
+
+maior_de_idade = [(idade) for idade in idades if idade > 21]
+print(maior_de_idade)
