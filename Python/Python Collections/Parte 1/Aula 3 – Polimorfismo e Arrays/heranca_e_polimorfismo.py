20b9c76acd9e0fa608771880030f13f9dea64ab9
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-10-02 13:53:36 -0300

    Método abstrato
    Aprendermos a utilizar e quais são suas vantagens

--- Python/Python Collections/Parte 1/Aula 3 – Polimorfismo e Arrays/heranca_e_polimorfismo.py
+++ Python/Python Collections/Parte 1/Aula 3 – Polimorfismo e Arrays/heranca_e_polimorfismo.py
@@ -1,4 +1,6 @@
-class Conta:
+from abc import ABCMeta, abstractmethod
+
+class Conta(metaclass=ABCMeta):
     def __init__(self, nome, codigo):
         self._nome = nome
         self._codigo = codigo
@@ -6,6 +8,10 @@ class Conta:
 
     def deposita(self, valor):
         self._saldo += valor
+
+    @abstractmethod
+    def passa_o_mes(self):
+        pass
 
     def __str__(self):
         return f'Conta de {self._nome}:\nCódigo: {self._codigo} Saldo: {self._saldo}'
@@ -15,21 +21,21 @@ class ContaCorrente(Conta):
     def passa_o_mes(self):
         self._saldo -= 2
 
-
 class ContaPoupanca(Conta):
     def passa_o_mes(self):
         self._saldo += 1.01
         self._saldo -= 3
 
-conta13 = ContaPoupanca('Vitor', 13)
-conta13.deposita(1000)
+class ContaInvestimento(Conta):
+    pass
+
 conta16 = ContaCorrente('Pedro', 16)
 conta16.deposita(1000)
-
-contas = [conta13, conta16]
+conta13 = ContaPoupanca('Vitor', 13)
+conta13.deposita(1000)
+# conta14 = ContaInvestimento('João', 14) Dará erro ao tentar executar
 
 for conta in contas:
     conta.passa_o_mes()
     print(conta, '\n')
-
 
