52668bddd338adbe6a8ddc6d4ce08409298646f5
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-10-22 13:55:17 -0300

    Refatorando o código
    Aprendemos como refatorar o código para deixar com uma leitura melhor e mais otimizado

--- Python/Validação de Dados no Padrão Nacional/Aula 2 – Validando CNPJ e Construindo uma Factory/cpf_cnpj.py
+++ Python/Validação de Dados no Padrão Nacional/Aula 2 – Validando CNPJ e Construindo uma Factory/cpf_cnpj.py
@@ -1,47 +1,52 @@
 from validate_docbr import CPF, CNPJ
 
-class CpfCnpj:
-    def __init__(self, documento, tipo_documento):
-        self.tipo_documento = tipo_documento
-        documento = str(documento)
-        if self.tipo_documento == 'cpf':
-            if self.cpf_e_valido(documento):
-                self.cpf = documento
-            else:
-                raise ValueError("CPF inválido!!")
-        elif self.tipo_documento == 'cnpj':
-            if self.cnpj_e_valido(documento):
-                self.cnpj = documento
-            else:
-                raise ValueError("CNPJ inválido!!")
-        else:
-            raise ValueError("Documento inválido!!")
 
+class Documento:
 
-    def cpf_e_valido(self, cpf):
-        if len(cpf) == 11:
-            validador = CPF()
-            return validador.validate(cpf)
+    @staticmethod
+    def cria_documento(documento):
+        if len(documento) == 11:
+            return DocCpf(documento)
+        elif len(documento) == 14:
+            return DocCnpj(documento)
         else:
             raise ValueError("Quantidade de dígitos inválida!!")
 
-    def format_cpf(self):
+class DocCpf:
+    def __init__(self, documento):
+        if self.valida(documento):
+            self.cpf = documento
+        else:
+            raise ValueError("CPF inválido!!")
+
+    def valida(self, documento):
+        validador = CPF()
+        return validador.validate(documento)
+
+    def format(self):
         mascara = CPF()
         return mascara.mask(self.cpf)
 
-    def format_cnpj(self):
+    def __str__(self):
+        return self.format()
+
+
+
+class DocCnpj():
+    def __init__(self, documento):
+        if self.valida(documento):
+            self.cnpj = documento
+        else:
+            raise ValueError("CNPJ inválido!!")
+
+    def valida(self, documento):
+        validador = CNPJ()
+        return validador.validate(documento)
+
+    def format(self):
         mascara = CNPJ()
         return mascara.mask(self.cnpj)
 
     def __str__(self):
-        if self.tipo_documento == 'cpf':
-            return self.format_cpf()
-        elif self.tipo_documento == 'cnpj':
-            return self.format_cnpj()
+        return self.format()
 
-    def cnpj_e_valido(self, cnpj):
-        if len(cnpj) == 14:
-            validate_cnpj = CNPJ()
-            return validate_cnpj.validate(cnpj)
-        else:
-            raise ValueError("Quantidade de dígitos inválida!!")
--- Python/Validação de Dados no Padrão Nacional/Aula 2 – Validando CNPJ e Construindo uma Factory/main.py
+++ Python/Validação de Dados no Padrão Nacional/Aula 2 – Validando CNPJ e Construindo uma Factory/main.py
@@ -1,8 +1,8 @@
-from cpf_cnpj import CpfCnpj
+from cpf_cnpj import Documento
 
 exemplo_cnpj = "35379838000112"
 exemplo_cpf = "46789082859"
 
-documento1 = CpfCnpj(exemplo_cpf, 'cpf')
-documento2 = CpfCnpj(exemplo_cnpj, 'cnpj')
+documento1 = Documento.cria_documento(exemplo_cpf)
+documento2 = Documento.cria_documento(exemplo_cnpj)
 print(f'{documento1}\n{documento2}')
