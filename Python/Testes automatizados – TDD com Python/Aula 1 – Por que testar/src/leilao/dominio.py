540543084a3658ae82c9afe7d3d328c22c107ec9
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-11-02 20:57:44 -0300

    implementando o avaliador de leilões
    Implementamos o avaliador e aprendemos sobre uma nova biblioteca (sys), além da importância de testar nosso código.

--- Python/Testes automatizados – TDD com Python/Aula 1 – Por que testar/src/leilao/dominio.py
+++ Python/Testes automatizados – TDD com Python/Aula 1 – Por que testar/src/leilao/dominio.py
@@ -1,5 +1,6 @@
-class Usuario:
+import sys
 
+class Usuario:
     def __init__(self, nome):
         self.__nome = nome
 
@@ -9,14 +10,12 @@ class Usuario:
 
 
 class Lance:
-
     def __init__(self, usuario, valor):
         self.usuario = usuario
         self.valor = valor
 
 
 class Leilao:
-
     def __init__(self, descricao):
         self.descricao = descricao
         self.__lances = []
@@ -24,3 +23,17 @@ class Leilao:
     @property
     def lances(self):
         return self.__lances
+
+
+class Avaliador:
+    def __init__(self):
+        self.maior_lance = sys.float_info.min
+        self.menor_lance = sys.float_info.max
+
+    def avalia(self, leilao: Leilao):
+        for lance in leilao.lances:
+            if lance.valor > self.maior_lance:
+                self.maior_lance = lance.valor
+            elif lance.valor < self.menor_lance:
+                self.menor_lance = lance.valor
+
--- Python/Testes automatizados – TDD com Python/Aula 1 – Por que testar/src/leilao/principal.py
+++ Python/Testes automatizados – TDD com Python/Aula 1 – Por que testar/src/leilao/principal.py
@@ -1,4 +1,4 @@
-from src.leilao.dominio import Usuario, Lance, Leilao
+from src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
 
 gui = Usuario('Gui')
 yuri = Usuario('Yuri')
@@ -13,3 +13,7 @@ leilao.lances.append(lance_do_yuri)
 
 for lance in leilao.lances:
     print(f'O usuário {lance.usuario.nome} deu umm lance de {lance.valor}')
+
+avaliador = Avaliador()
+avaliador.avalia(leilao)
+print(f'O menor lance foi de {avaliador.menor_lance} e o maior lance foi de {avaliador.maior_lance}')
