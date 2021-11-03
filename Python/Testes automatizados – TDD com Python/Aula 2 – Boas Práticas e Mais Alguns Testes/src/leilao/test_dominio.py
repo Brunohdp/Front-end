f0fba38ea5b84fec87852ac9183a6832251a100a
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-11-02 22:27:28 -0300

    Isolando a criação do cenário
    Aprendemos sobre o método setup e as boas práticas para quando e como utilizá-lo

--- Python/Testes automatizados – TDD com Python/Aula 2 – Boas Práticas e Mais Alguns Testes/src/leilao/test_dominio.py
+++ Python/Testes automatizados – TDD com Python/Aula 2 – Boas Práticas e Mais Alguns Testes/src/leilao/test_dominio.py
@@ -1,23 +1,23 @@
 from unittest import TestCase
-
 from src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
 
 
 class TestAvaliador(TestCase):
+    def setUp(self):
+        self.gui = Usuario('Gui')
+        self.lance_do_gui = Lance(self.gui, 150.0)
+        self.leilao = Leilao('Celular')
+
+
     def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
-        gui = Usuario('Gui')
         yuri = Usuario('Yuri')
-
         lance_do_yuri = Lance(yuri, 100.0)
-        lance_do_gui = Lance(gui, 150.0)
 
-        leilao = Leilao('Celular')
-
-        leilao.lances.append(lance_do_yuri)
-        leilao.lances.append(lance_do_gui)
+        self.leilao.lances.append(lance_do_yuri)
+        self.leilao.lances.append(self.lance_do_gui)
 
         avaliador = Avaliador()
-        avaliador.avalia(leilao)
+        avaliador.avalia(self.leilao)
 
         menor_valor_esperado = 100.0
         maior_valor_esperado = 150.0
@@ -26,19 +26,14 @@ class TestAvaliador(TestCase):
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
 
     def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_decrescente(self):
-        gui = Usuario('Gui')
         yuri = Usuario('Yuri')
-
         lance_do_yuri = Lance(yuri, 100.0)
-        lance_do_gui = Lance(gui, 150.0)
 
-        leilao = Leilao('Celular')
-
-        leilao.lances.append(lance_do_gui)
-        leilao.lances.append(lance_do_yuri)
+        self.leilao.lances.append(self.lance_do_gui)
+        self.leilao.lances.append(lance_do_yuri)
 
         avaliador = Avaliador()
-        avaliador.avalia(leilao)
+        avaliador.avalia(self.leilao)
 
         menor_valor_esperado = 100.0
         maior_valor_esperado = 150.0
@@ -47,32 +42,24 @@ class TestAvaliador(TestCase):
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
 
     def test_deve_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_o_leilao_tiver_um_lance(self):
-        gui = Usuario('Gui')
-
-        lance = Lance(gui, 150.0)
-
-        leilao = Leilao('Celular')
-
-        leilao.lances.append(lance)
+        self.leilao.lances.append(self.lance_do_gui)
 
         avaliador = Avaliador()
-        avaliador.avalia(leilao)
+        avaliador.avalia(self.leilao)
 
         self.assertEqual(150.0, avaliador.menor_lance)
         self.assertEqual(150.0, avaliador.maior_lance)
 
     def test_deve_retornar_o_maior_e_menor_valor_quando_o_leilao_tiver_tres_lances(self):
-        bru = Usuario('Bru')
-        gui = Usuario('Gui')
         yuri = Usuario('Yuri')
+        bru = Usuario('Bru')
 
         lance_do_yuri = Lance(yuri, 100.0)
-        lance_do_gui = Lance(gui, 150.0)
         lance_do_bru = Lance(bru, 200.0)
 
         leilao = Leilao('Celular')
 
-        leilao.lances.append(lance_do_gui)
+        leilao.lances.append(self.lance_do_gui)
         leilao.lances.append(lance_do_yuri)
         leilao.lances.append(lance_do_bru)
 
