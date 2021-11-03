2fd621da834864c98f65de475ff8a3ca4d89eeea
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-11-02 21:30:25 -0300

    Criando um novo teste
    Copiamos o primeiro teste e colamos logo abaixo na mesma classe, apenas invertendo a ordem dos lances, para ver se teria alguma falha

--- Python/Testes automatizados – TDD com Python/Aula 2 – Boas Práticas e Mais Alguns Testes/src/leilao/test_dominio.py
+++ Python/Testes automatizados – TDD com Python/Aula 2 – Boas Práticas e Mais Alguns Testes/src/leilao/test_dominio.py
@@ -24,3 +24,24 @@ class TestAvaliador(TestCase):
 
         self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+
+    def test_avalia2(self):
+        gui = Usuario('Gui')
+        yuri = Usuario('Yuri')
+
+        lance_do_yuri = Lance(yuri, 100.0)
+        lance_do_gui = Lance(gui, 150.0)
+
+        leilao = Leilao('Celular')
+
+        leilao.lances.append(lance_do_gui)
+        leilao.lances.append(lance_do_yuri)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 150.0
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
