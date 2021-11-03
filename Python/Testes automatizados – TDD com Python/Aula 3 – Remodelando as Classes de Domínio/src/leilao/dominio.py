af152f5b38d898717b0d45d7434d1e6871ee4ebd
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-11-03 09:19:45 -0300

    Estado e comportamento
    Aprendemos quando e porque separar funcionalidades em objetos diferentes, além de como remover um deles e mesclar com outro.

--- Python/Testes automatizados – TDD com Python/Aula 3 – Remodelando as Classes de Domínio/src/leilao/dominio.py
+++ Python/Testes automatizados – TDD com Python/Aula 3 – Remodelando as Classes de Domínio/src/leilao/dominio.py
@@ -19,8 +19,14 @@ class Leilao:
     def __init__(self, descricao):
         self.descricao = descricao
         self.__lances = []
+        self.maior_lance = sys.float_info.min
+        self.menor_lance = sys.float_info.max
 
     def propoem(self, lance: Lance):
+        if lance.valor > self.maior_lance:
+            self.maior_lance = lance.valor
+        if lance.valor < self.menor_lance:
+            self.menor_lance = lance.valor
         self.__lances.append(lance)
 
     @property
@@ -28,15 +34,4 @@ class Leilao:
         return self.__lances[:]
 
 
-class Avaliador:
-    def __init__(self):
-        self.maior_lance = sys.float_info.min
-        self.menor_lance = sys.float_info.max
-
-    def avalia(self, leilao: Leilao):
-        for lance in leilao.lances:
-            if lance.valor > self.maior_lance:
-                self.maior_lance = lance.valor
-            if lance.valor < self.menor_lance:
-                self.menor_lance = lance.valor
 
--- Python/Testes automatizados – TDD com Python/Aula 3 – Remodelando as Classes de Domínio/src/leilao/test_dominio.py
+++ Python/Testes automatizados – TDD com Python/Aula 3 – Remodelando as Classes de Domínio/src/leilao/test_dominio.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
+from src.leilao.dominio import Usuario, Lance, Leilao
 
 
 class TestAvaliador(TestCase):
@@ -16,14 +16,12 @@ class TestAvaliador(TestCase):
         self.leilao.propoem(lance_do_yuri)
         self.leilao.propoem(self.lance_do_gui)
 
-        avaliador = Avaliador()
-        avaliador.avalia(self.leilao)
-
         menor_valor_esperado = 100.0
         maior_valor_esperado = 150.0
 
-        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
-        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+
 
     def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_decrescente(self):
         yuri = Usuario('Yuri')
@@ -32,23 +30,17 @@ class TestAvaliador(TestCase):
         self.leilao.propoem(self.lance_do_gui)
         self.leilao.propoem(lance_do_yuri)
 
-        avaliador = Avaliador()
-        avaliador.avalia(self.leilao)
-
         menor_valor_esperado = 100.0
         maior_valor_esperado = 150.0
 
-        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
-        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
     def test_deve_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_o_leilao_tiver_um_lance(self):
         self.leilao.propoem(self.lance_do_gui)
 
-        avaliador = Avaliador()
-        avaliador.avalia(self.leilao)
-
-        self.assertEqual(150.0, avaliador.menor_lance)
-        self.assertEqual(150.0, avaliador.maior_lance)
+        self.assertEqual(150.0, self.leilao.menor_lance)
+        self.assertEqual(150.0, self.leilao.maior_lance)
 
     def test_deve_retornar_o_maior_e_menor_valor_quando_o_leilao_tiver_tres_lances(self):
         yuri = Usuario('Yuri')
@@ -57,19 +49,14 @@ class TestAvaliador(TestCase):
         lance_do_yuri = Lance(yuri, 100.0)
         lance_do_bru = Lance(bru, 200.0)
 
-        leilao = Leilao('Celular')
-
-        leilao.propoem(self.lance_do_gui)
-        leilao.propoem(lance_do_yuri)
-        leilao.propoem(lance_do_bru)
-
-        avaliador = Avaliador()
-        avaliador.avalia(leilao)
+        self.leilao.propoem(self.lance_do_gui)
+        self.leilao.propoem(lance_do_yuri)
+        self.leilao.propoem(lance_do_bru)
 
         menor_valor_esperado = 100.0
         maior_valor_esperado = 200.0
 
-        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
-        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
 
