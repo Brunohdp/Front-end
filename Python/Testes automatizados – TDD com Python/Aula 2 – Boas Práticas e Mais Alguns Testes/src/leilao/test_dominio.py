fdcbf3b7137f1bdb0e8d06a3d67683498bad5c8f
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-11-02 21:42:28 -0300

    Renomeando os testes
    Aprendemos nomenclaturas de testes e sua importância

--- Python/Testes automatizados – TDD com Python/Aula 2 – Boas Práticas e Mais Alguns Testes/src/leilao/test_dominio.py
+++ Python/Testes automatizados – TDD com Python/Aula 2 – Boas Práticas e Mais Alguns Testes/src/leilao/test_dominio.py
@@ -4,7 +4,7 @@ from src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
 
 
 class TestAvaliador(TestCase):
-    def test_avalia(self):
+    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
         gui = Usuario('Gui')
         yuri = Usuario('Yuri')
 
@@ -25,7 +25,7 @@ class TestAvaliador(TestCase):
         self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
 
-    def test_avalia2(self):
+    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_decrescente(self):
         gui = Usuario('Gui')
         yuri = Usuario('Yuri')
 
@@ -45,3 +45,7 @@ class TestAvaliador(TestCase):
 
         self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+
+
+
+
