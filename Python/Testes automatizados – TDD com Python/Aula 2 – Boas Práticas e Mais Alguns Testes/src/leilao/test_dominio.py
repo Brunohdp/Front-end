4516ef71d518a31d6b53132804b0a96753be70f1
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-11-02 21:57:16 -0300

    Mais testes e classes de equivalência
    Aprendemos sobre teste de equivalência e que não precisamos criar infinitos testes, podemos ter confiança de que nosso código funciona se tivermos feito os testes equivalentes

--- Python/Testes automatizados – TDD com Python/Aula 2 – Boas Práticas e Mais Alguns Testes/src/leilao/test_dominio.py
+++ Python/Testes automatizados – TDD com Python/Aula 2 – Boas Práticas e Mais Alguns Testes/src/leilao/test_dominio.py
@@ -46,6 +46,43 @@ class TestAvaliador(TestCase):
         self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
 
+    def test_deve_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_o_leilao_tiver_um_lance(self):
+        gui = Usuario('Gui')
 
+        lance = Lance(gui, 150.0)
+
+        leilao = Leilao('Celular')
+
+        leilao.lances.append(lance)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        self.assertEqual(150.0, avaliador.menor_lance)
+        self.assertEqual(150.0, avaliador.maior_lance)
+
+    def test_deve_retornar_o_maior_e_menor_valor_quando_o_leilao_tiver_tres_lances(self):
+        bru = Usuario('Bru')
+        gui = Usuario('Gui')
+        yuri = Usuario('Yuri')
+
+        lance_do_yuri = Lance(yuri, 100.0)
+        lance_do_gui = Lance(gui, 150.0)
+        lance_do_bru = Lance(bru, 200.0)
+
+        leilao = Leilao('Celular')
+
+        leilao.lances.append(lance_do_gui)
+        leilao.lances.append(lance_do_yuri)
+        leilao.lances.append(lance_do_bru)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 200.0
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
 
 
