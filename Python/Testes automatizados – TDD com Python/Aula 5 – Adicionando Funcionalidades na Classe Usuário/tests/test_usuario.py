19f979aaa4da8d238f083002d0af21c01ad43da7
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-11-04 20:58:04 -0300

    Um pouco mais de testes
    Criamos mais alguns testes parecidos, mas com muita cópia de trecho, vamos consertar isso na próxima aula

--- Python/Testes automatizados – TDD com Python/Aula 5 – Adicionando Funcionalidades na Classe Usuário/tests/test_usuario.py
+++ Python/Testes automatizados – TDD com Python/Aula 5 – Adicionando Funcionalidades na Classe Usuário/tests/test_usuario.py
@@ -1,5 +1,7 @@
 from src.leilao.dominio import Usuario, Leilao
 
+
+
 def test_deve_subtrair_valor_da_carteira_do_usuario_quando_este_propor_um_lance():
     vini = Usuario('Vini', 100.0)
 
@@ -8,3 +10,33 @@ def test_deve_subtrair_valor_da_carteira_do_usuario_quando_este_propor_um_lance(
     vini.propoem_lance(leilao, 50.0)
 
     assert vini.carteira == 50.0
+
+
+def test_deve_permitir_propor_lance_quando_o_valor_e_menor_que_o_valor_da_carteira():
+    vini = Usuario('Vini', 100.0)
+
+    leilao = Leilao('Celular')
+
+    vini.propoem_lance(leilao, 1.0)
+
+    assert vini.carteira == 99.0
+
+
+def test_deve_permitir_propor_lance_quando_o_valor_e_igual_ao_valor_da_carteira():
+    vini = Usuario('Vini', 100.0)
+
+    leilao = Leilao('Celular')
+
+    vini.propoem_lance(leilao, 100.0)
+
+    assert vini.carteira == 0.0
+
+
+def test_nao_deve_permitir_propor_lance_com_valor_maior_que_o_da_carteira():
+    vini = Usuario('Vini', 100.0)
+
+    leilao = Leilao('Celular')
+
+    vini.propoem_lance(leilao, 150.0)
+
+    assert vini.carteira == 100.0
