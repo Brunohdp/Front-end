de4f2e60e54f3562924d4dc545166d606ccca109
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-11-04 22:06:57 -0300

    Isolando as condições
    Aprendemos sobre refatorar condições e exceções, suas práticas e como deixa o código muito mais legível e de f´´acil entendimento.

--- Python/Testes automatizados – TDD com Python/Aula 6 – Refatorando o Projeto/src/leilao/dominio.py
+++ Python/Testes automatizados – TDD com Python/Aula 6 – Refatorando o Projeto/src/leilao/dominio.py
@@ -1,12 +1,10 @@
-import sys
-
 class Usuario:
     def __init__(self, nome, carteira):
         self.__nome = nome
         self.__carteira = carteira
 
     def propoem_lance(self, leilao, valor):
-        if valor > self.__carteira:
+        if self._valor_eh_valido(valor):
             raise ValueError('Não pode propor um lance com valor maior que o valor da carteira!')
         else:
             lance = Lance(self, valor)
@@ -22,6 +20,9 @@ class Usuario:
     def carteira(self):
         return self.__carteira
 
+    def _valor_eh_valido(self, valor):
+        return valor > self.__carteira
+
 
 class Lance:
     def __init__(self, usuario, valor):
@@ -33,15 +34,16 @@ class Leilao:
     def __init__(self, descricao):
         self.descricao = descricao
         self.__lances = []
-        self.maior_lance = sys.float_info.min
-        self.menor_lance = sys.float_info.max
+        self.maior_lance = 0.0
+        self.menor_lance = 0.0
 
     def propoem(self, lance: Lance):
-        if not self.__lances or self.__lances[-1].usuario != lance.usuario and lance.valor > self.__lances[-1].valor:
-            if lance.valor > self.maior_lance:
-                self.maior_lance = lance.valor
-            if lance.valor < self.menor_lance:
+        if self._lance_eh_valido(lance):
+            if not self._tem_lances():
                 self.menor_lance = lance.valor
+
+            self.menor_lance = lance.valor
+
             self.__lances.append(lance)
         else:
             raise ValueError('Erro ao Propor Lance')
@@ -49,6 +51,19 @@ class Leilao:
     @property
     def lances(self):
         return self.__lances[:]
+
+    def _tem_lances(self):
+        return self.__lances
+
+    def _usuarios_diferentes(self, lance):
+        return self.__lances[-1].usuario != lance.usuario
+
+    def _valor_maior_que_anterior(self, lance):
+        return lance.valor > self.__lances[-1].valor
+
+    def _lance_eh_valido(self, lance):
+        return not self._tem_lances() or (self._usuarios_diferentes(lance) and
+                                          self._valor_maior_que_lance_anterior(lance))
 
 
 
