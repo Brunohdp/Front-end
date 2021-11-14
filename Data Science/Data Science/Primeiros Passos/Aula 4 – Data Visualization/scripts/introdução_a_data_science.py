fb2bdd844d5f9ffe8cb699567fd3a09fee1ddc76
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2021-11-14 19:50:09 -0300

    Conteúdo aula 4
    baixei meus scripts e coloquei na pasta

--- Data Science/Data Science/Primeiros Passos/Aula 4 – Data Visualization/scripts/introdução_a_data_science.py
+++ Data Science/Data Science/Primeiros Passos/Aula 4 – Data Visualization/scripts/introdução_a_data_science.py
@@ -106,3 +106,58 @@ tmdb['original_language'].unique()
 # Quantidade de votos => 1, 2, 3, 4, não tem 2.5 votos.
 # notas do movielens => 0.5, 1, 1.5, ... ,5 não tem 2.7
 
+"""# Aula 4 – Data Visualization
+
+## Visualizando gráficos por categoria
+"""
+
+tmdb['original_language'].value_counts()
+
+tmdb['original_language'].value_counts().index
+
+tmdb['original_language'].value_counts().values
+
+tmdb['original_language'].value_counts().to_frame()
+
+contagem_de_lingua = tmdb['original_language'].value_counts().to_frame().reset_index()
+contagem_de_lingua.columns = ['original_language', 'total']
+contagem_de_lingua.head()
+
+sns.barplot(x = 'original_language', y = 'total', data = contagem_de_lingua)
+
+print(sns.__version__)
+
+!pip install seaborn=0.9.0
+
+sns.catplot(x = 'original_language', kind='count', data = tmdb)
+
+"""## Passando uma mensagem através de visualização"""
+
+plt.pie(contagem_de_lingua['total'], labels = contagem_de_lingua['original_language'])
+plt.show()
+
+total_por_lingua = tmdb['original_language'].value_counts()
+total_geral = sum(total_por_lingua)
+total_de_ingles = total_por_lingua.loc['en']
+total_do_resto = total_geral - total_de_ingles
+print(total_de_ingles, total_do_resto)
+
+dados = {
+    'lingua': ['ingles', 'outros'],
+    'total': [total_de_ingles, total_do_resto]
+}
+dados = pd.DataFrame(dados)
+dados
+
+sns.barplot(x = 'lingua', y = 'total', data = dados)
+
+"""## Visualizando as outras categorias"""
+
+plt.pie(dados['total'], labels = dados['lingua'])
+
+total_por_lingua_de_outros_filmes = tmdb.query("original_language != 'en'").original_language.value_counts()
+total_por_lingua_de_outros_filmes
+
+filmes_sem_lingua_original_em_ingles = tmdb.query("original_language != 'en'")
+sns.catplot(x = 'original_language', kind = 'count', data = filmes_sem_lingua_original_em_ingles)
+
