814603a5e3164ac9757e0cae6da3e53e9969c2cc
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2022-02-09 16:01:36 -0300

    Funções com parâmetros
    Aprendemos o que são e como usamos parâmetros em funções

--- Front-end/Formação HTML && CSS3/JavaScript na Web/Crie Páginas Dinâmicas/Aula 4 – Iterando Em Listas/main.js
+++ Front-end/Formação HTML && CSS3/JavaScript na Web/Crie Páginas Dinâmicas/Aula 4 – Iterando Em Listas/main.js
@@ -1,6 +1,5 @@
-// Pom --------------------------------------------
-function tocaSomPom(){
-  document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -8,9 +7,7 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 while (contador < listaDeTeclas.length){
-  listaDeTeclas[contador].onclick = tocaSomPom;
+  listaDeTeclas[contador].onclick = tocaSom;
   
   contador = (contador + 1);
-
-  console.log(contador);
 }
