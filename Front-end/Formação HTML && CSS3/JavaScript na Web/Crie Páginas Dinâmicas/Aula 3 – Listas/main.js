bd717fb6642eb7b3d9c333c57f4234826cf31c91
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2022-02-09 14:54:43 -0300

    Ambiente preparado

---
+++ Front-end/Formação HTML && CSS3/JavaScript na Web/Crie Páginas Dinâmicas/Aula 3 – Listas/main.js
@@ -0,0 +1,9 @@
+function tocaSomPom(){
+  document.querySelector('#som_tecla_pom').play();
+}
+
+function tocaSomClap(){
+  document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
