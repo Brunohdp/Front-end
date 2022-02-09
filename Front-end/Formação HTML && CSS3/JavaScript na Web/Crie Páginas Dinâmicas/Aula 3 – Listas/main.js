1517ae3d4ceeea071bb9fb27b215bacadcc32dc5
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2022-02-09 15:05:46 -0300

    Lista de Elementos
    Aprendemos a usar o querySelectorAll('') para trazer uma lista com todos os itens que possuam uma mesma classe.

--- Front-end/Formação HTML && CSS3/JavaScript na Web/Crie Páginas Dinâmicas/Aula 3 – Listas/main.js
+++ Front-end/Formação HTML && CSS3/JavaScript na Web/Crie Páginas Dinâmicas/Aula 3 – Listas/main.js
@@ -2,8 +2,4 @@ function tocaSomPom(){
   document.querySelector('#som_tecla_pom').play();
 }
 
-function tocaSomClap(){
-  document.querySelector('#som_tecla_clap').play();
-}
-
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+document.querySelectorAll('.tecla').onclick = tocaSomPom;
