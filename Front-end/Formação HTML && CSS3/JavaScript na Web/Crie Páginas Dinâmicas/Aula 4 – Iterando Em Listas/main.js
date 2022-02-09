d7ec462f367d09f0ea5a87fa9640781d91695ce1
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2022-02-09 16:27:59 -0300

    Repetição Otimizada Com For
    Aprendemos como usar o for e suas vantagens com relação ao while

--- Front-end/Formação HTML && CSS3/JavaScript na Web/Crie Páginas Dinâmicas/Aula 4 – Iterando Em Listas/main.js
+++ Front-end/Formação HTML && CSS3/JavaScript na Web/Crie Páginas Dinâmicas/Aula 4 – Iterando Em Listas/main.js
@@ -3,16 +3,13 @@ function tocaSom(idElementoAudio) {
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0;
 
-while (contador < listaDeTeclas.length){
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
   const tecla = listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
 
-  //tamplate string
-  const idAudio = `#som_${instrumento}`;
+  const idAudio = `#som_${instrumento}`; //tamplate string
 
   tecla.onclick = function(){tocaSom(idAudio)};
-  
-  contador = (contador + 1);
 }
