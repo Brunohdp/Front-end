f730e01edef7d26fc4595c1a26cfb44ff95467ff
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2022-02-09 15:51:54 -0300

    Percorrendo Uma Lista
    Aprendemos a percorrer a lista com o while

--- Front-end/Formação HTML && CSS3/JavaScript na Web/Crie Páginas Dinâmicas/Aula 4 – Iterando Em Listas/main.js
+++ Front-end/Formação HTML && CSS3/JavaScript na Web/Crie Páginas Dinâmicas/Aula 4 – Iterando Em Listas/main.js
@@ -5,4 +5,12 @@ function tocaSomPom(){
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
+while (contador < listaDeTeclas.length){
+  listaDeTeclas[contador].onclick = tocaSomPom;
+  
+  contador = (contador + 1);
+
+  console.log(contador);
+}
