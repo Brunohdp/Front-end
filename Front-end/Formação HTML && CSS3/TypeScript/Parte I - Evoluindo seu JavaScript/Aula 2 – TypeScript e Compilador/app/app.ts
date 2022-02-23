480a2cdd61bb8a3a66bb97eecd569312b16f4337
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2022-02-23 12:45:01 -0300

    Configuração básica do compilador
    Comecei a configuração, mas não vou terminar. Não funciona essa merda e já fiquei puto. Desde manhã tentando concertar esse krl e não vai. Depois volto nessa merda

--- Front-end/Formação HTML && CSS3/TypeScript/Parte I - Evoluindo seu JavaScript/Aula 2 – TypeScript e Compilador/app/app.ts
+++ Front-end/Formação HTML && CSS3/TypeScript/Parte I - Evoluindo seu JavaScript/Aula 2 – TypeScript e Compilador/app/app.ts
@@ -1,5 +1,5 @@
 // Módulo do APP
 import { Negociacao } from "./models/negociacao.js";
 
-const negociacao = new Negociacao(new Date(), 10, 100);
+const negociacao = new Negociacao(new Date());
 console.log(negociacao.volume);
