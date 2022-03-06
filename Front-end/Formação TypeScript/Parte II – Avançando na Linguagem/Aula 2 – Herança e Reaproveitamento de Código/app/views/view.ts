7d4675b0f368d7fd56be5d36e165fc5d8e1e06ef
Brunohdp <bruno.h.d3v@gmail.com>
2022-03-06 12:21:06 -0300

    Mais surpresas com Herança
    Vimos mais algumas surpresas que ocorrem devido a tipagem dos métodos.

--- Front-end/Formação TypeScript/Parte II – Avançando na Linguagem/Aula 2 – Herança e Reaproveitamento de Código/app/views/view.ts
+++ Front-end/Formação TypeScript/Parte II – Avançando na Linguagem/Aula 2 – Herança e Reaproveitamento de Código/app/views/view.ts
@@ -4,4 +4,13 @@ export class View {
   constructor(seletor:string) {
     this.elemento = document.querySelector(seletor);
   }
+
+  template(model:string): string {
+    throw Error('A classe filha deve implementar o método template')
+  }
+
+  update(model:string): void {
+    const template = this.template(model);
+    this.elemento.innerHTML = template;
+  }
 }
