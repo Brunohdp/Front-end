51f7c3afe9efe5c49d483a9f366298d03b67ef30
Brunohdp <bruno.h.d3v@gmail.com>
2022-03-12 16:53:33 -0300

    Cuidados na declaração de enums
    Vimos alguns cuidados que temos ter e como evitar erros a partir deles nas enums

--- Front-end/Formação TypeScript/Parte II – Avançando na Linguagem/Aula 3 – Visibilidade de Métodos e Enumeration/app/enums/dias-da-semana.ts
+++ Front-end/Formação TypeScript/Parte II – Avançando na Linguagem/Aula 3 – Visibilidade de Métodos e Enumeration/app/enums/dias-da-semana.ts
@@ -1,3 +1,9 @@
 export enum DiasDaSemana {
-  DOMINGO, SEGUNDA, TERÇA, QUARTA, QUINTA, SEXTA, SABADO
+  DOMINGO = 0,
+  SEGUNDA = 1,
+  TERÇA = 2,
+  QUARTA = 3,
+  QUINTA = 4,
+  SEXTA = 5,
+  SABADO = 6
 }
