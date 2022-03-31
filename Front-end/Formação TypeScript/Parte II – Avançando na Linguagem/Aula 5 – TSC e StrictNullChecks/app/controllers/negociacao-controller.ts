bc7f8f8e9d371204e1f2747fa8d4030ad24ec311
Brunohdp <bruno.h.d3v@gmail.com>
2022-03-31 18:44:46 -0300

    Ambiente preparado

---
+++ Front-end/Formação TypeScript/Parte II – Avançando na Linguagem/Aula 5 – TSC e StrictNullChecks/app/controllers/negociacao-controller.ts
@@ -0,0 +1,54 @@
+import { DiasDaSemana } from "../enums/dias-da-semana.js";
+import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
+import { MensagemView } from "../views/mensagem-view.js";
+import { NegociacoesView } from "../views/negociacoes-view.js";
+
+export class NegociacaoController {
+  private inputData: HTMLInputElement;
+  private inputQuantidade: HTMLInputElement;
+  private inputValor: HTMLInputElement;
+  private negociacoes = new Negociacoes();
+  private negociacoesView = new NegociacoesView('#negociacoesView', true);
+  private mensagemView = new MensagemView('#mensagemView');
+  
+
+  constructor() {
+    this.inputData = document.querySelector('#data');
+    this.inputQuantidade = document.querySelector('#quantidade');
+    this.inputValor = document.querySelector('#valor');
+    this.negociacoesView.update(this.negociacoes);
+  }
+
+  public adiciona(): void {
+    const negociacao = Negociacao.criaDe(
+      this.inputData.value,
+      this.inputQuantidade.value,
+      this.inputValor.value
+    );
+    if (this.ehDiaUtil(negociacao.data)){
+      this.negociacoes.adiciona(negociacao);
+      console.log(this.negociacoes.lista());
+      this.limparFormulario();
+      this.atualizaView();
+    } else {
+      this.mensagemView.update("Apenas negociações em dias úteis são aceitas");
+    }
+  }
+
+  private ehDiaUtil(data: Date): boolean {
+    return data.getDay() > DiasDaSemana.DOMINGO && data.getDay() < DiasDaSemana.SABADO
+  }
+
+  private limparFormulario(): void {
+    this.inputData.value = '';
+    this.inputQuantidade.value = '';
+    this.inputValor.value = '';
+    this.inputData.focus();
+  }
+
+  private atualizaView(): void {
+    this.negociacoesView.update(this.negociacoes);
+    this.mensagemView.update('Negociação adicionada com sucesso!');
+  }
+}
