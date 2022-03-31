8548a844b5a4dc191834c98b16719f5e8ef95bca
Brunohdp <bruno.h.d3v@gmail.com>
2022-03-30 21:46:25 -0300

    Parâmetros opcionais
    Aprendemos sobre parâmetros opcionais e como utilizar eles no projeto

--- Front-end/Formação TypeScript/Parte II – Avançando na Linguagem/Aula 4 – Lapidando Nosso Código/app/views/view.ts
+++ Front-end/Formação TypeScript/Parte II – Avançando na Linguagem/Aula 4 – Lapidando Nosso Código/app/views/view.ts
@@ -2,8 +2,11 @@ export abstract class View<T> {
   protected elemento: HTMLElement;
   private escapar = false;
 
-  constructor(seletor:string, escapar: boolean) {
+  constructor(seletor:string, escapar?: boolean) {
     this.elemento = document.querySelector(seletor);
+    if (escapar) {
+      this.escapar = escapar;
+    }
   }
 
   protected abstract template(model: T): string;
