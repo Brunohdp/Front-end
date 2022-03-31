1cb755934d5d9f57dcd8ca2b81496cd650fbd3f5
Brunohdp <bruno.h.d3v@gmail.com>
2022-03-31 19:17:34 -0300

    Tratando null em nosso código
    Tramamos com if

--- Front-end/Formação TypeScript/Parte II – Avançando na Linguagem/Aula 5 – TSC e StrictNullChecks/app/app.ts
+++ Front-end/Formação TypeScript/Parte II – Avançando na Linguagem/Aula 5 – TSC e StrictNullChecks/app/app.ts
@@ -3,7 +3,11 @@ import { NegociacoesView } from "./views/negociacoes-view.js";
 
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
+if (form) {
 form.addEventListener('submit', event => {
   event.preventDefault();
   controller.adiciona();
 });
+} else {
+  throw Error('Não foi possível inicializar a aplicação. Verifique se o código existe.')
+}
