a82cf7277d90e6f3fdf6eb8eaadee823f51babe5
Bruno Henrique de Paula <78300872+Brunohdp@users.noreply.github.com>
2022-07-29 14:08:31 -0300

    Aula 3
    Finalizada

--- JavaScript para Front-end/JavaScript - Manipulando o DOM/Aula 1 - JavaScript para Web/js/main.js
+++ JavaScript para Front-end/JavaScript - Manipulando o DOM/Aula 1 - JavaScript para Web/js/main.js
@@ -1,12 +1,18 @@
-const robotron = document.querySelector("#robotron")
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
 
-robotron.addEventListener("click", () => {
-  console.log("Robotron was clicked")
-});
+const controle = document.querySelectorAll(".controle-ajuste")
 
-function dizOi(nome) {
-  console.log("Oi, " + nome);
-  console.log("Bem-vindo ao Robotron 2000!");
+controle.forEach((elemento) => { 
+  elemento.addEventListener("click", (event) => {
+    manipulaDados(event.target.textContent)
+  })})
+
+function manipulaDados(operacao) {
+  if (operacao === "+") {
+    braco.value = parseInt(braco.value) + 1;
+  } else {
+    braco.value = parseInt(braco.value) - 1;
+  }
 }
-
-dizOi("Bruno");
